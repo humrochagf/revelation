58659ccf996616b77af94e66aa6c6eb940b74f43
Humberto Rocha <humrochagf@gmail.com>
2018-04-01 18:49:33 -0300

    Add custom config test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import tempfile
 import unittest
 
 from revelation.config import Config
@@ -8,7 +9,18 @@ from revelation.config import Config
 class ConfigTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.config = Config()
+        _, config_file = tempfile.mkstemp('.py')
+
+        with open(config_file, 'w') as file:
+            file.write(
+                'REVEAL_META = {\n'
+                '"title": "Test Title",\n'
+                '"author": "Test Author",\n'
+                '"description": "Test description",\n'
+                '}'
+            )
+
+        self.config = Config(config_file)
 
     def test_config_extends_dict(self):
         self.assertIsInstance(self.config, dict)
@@ -16,4 +28,15 @@ class ConfigTestCase(unittest.TestCase):
     def test_config_initialize_variables(self):
         self.assertIn('REVEAL_META', self.config)
         self.assertIn('REVEAL_SLIDE_SEPARATOR', self.config)
+        self.assertIn('REVEAL_THEME', self.config)
         self.assertIn('REVEAL_CONFIG', self.config)
+
+    def test_config_custom_values(self):
+        self.assertDictEqual(
+            self.config['REVEAL_META'],
+            {
+                'title': 'Test Title',
+                'author': 'Test Author',
+                'description': 'Test description',
+            }
+        )
