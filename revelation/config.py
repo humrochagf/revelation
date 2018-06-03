0314c028b6b9e0551cf41bd841ab297dc6a16515
Humberto Rocha <humrochagf@gmail.com>
2018-06-02 21:31:54 -0300

    Improve redability at config

--- revelation/config.py
+++ revelation/config.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Revelation configuration handler"""
 
 import os
 import types
@@ -19,12 +20,16 @@ class Config(dict):
     """
 
     def __init__(self, filename=None):
+        """Initializes the config with the defaults or with custom
+        variables from an external file"""
         self.load_from_object(default_config)
 
         if filename and os.path.isfile(filename):
             self.load_from_pyfile(filename)
 
     def load_from_object(self, obj):
+        """Load the configs from a python object passed
+        to the function"""
         if isinstance(obj, str):
             obj = import_string(obj)
 
@@ -33,14 +38,21 @@ class Config(dict):
                 self[key] = getattr(obj, key)
 
     def load_from_pyfile(self, filename):
-        d = types.ModuleType("config")
-        d.__file__ = filename
+        """Load the configs from a python file as it was imported"""
+        module = types.ModuleType("config")
+        module.__file__ = filename
 
         try:
             with open(filename, mode="rb") as config_file:
-                exec(compile(config_file.read(), filename, "exec"), d.__dict__)
-        except IOError as e:
-            e.strerror = "Unable to load configuration file (%s)" % e.strerror
+                exec(
+                    compile(config_file.read(), filename, "exec"),
+                    module.__dict__
+                )
+        except IOError as error:
+            error.strerror = "Unable to load configuration file ({})".format(
+                error.strerror
+            )
+
             raise
 
-        self.load_from_object(d)
+        self.load_from_object(module)
