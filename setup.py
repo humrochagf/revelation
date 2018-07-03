4e3101df8a2420e592eb8353e91da22c95d3a38e
Humberto Rocha <humrochagf@gmail.com>
2018-07-03 11:08:05 -0300

    Keep python 3.7 out until its build process estabilize

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Topic :: Multimedia :: Graphics :: Presentation",
         "Topic :: Text Processing :: Markup :: HTML",
     ],
