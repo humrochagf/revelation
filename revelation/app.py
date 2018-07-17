de4f8221e6c78aca174600dd333b0f9a5f62baa2
Daniele Teti <d.teti@bittime.it>
2018-07-17 20:05:17 +0200

    Added support for unicode languages in markdown

--- revelation/app.py
+++ revelation/app.py
@@ -78,7 +78,7 @@ class Revelation(object):
 
         :return: a list of strings with the slides content
         """
-        with open(path, "r") as presentation:
+        with open(path, "r", encoding="utf-8") as presentation:
             slides = presentation.read()
 
         return re.split("^{}$".format(separator), slides, flags=re.MULTILINE)
@@ -112,7 +112,7 @@ class Revelation(object):
         template = env.get_template("presentation.html")
 
         return Response(
-            template.render(**context), headers={"content-type": "text/html"}
+            template.render(**context), headers={"content-type": "text/html; charset=utf8"}
         )
 
     def wsgi_app(self, environ, start_response):
