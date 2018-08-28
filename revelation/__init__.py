7deb809b46cb3c90a98af7a846da7a81299cb3d5
Humberto Rocha <humrochagf@gmail.com>
2018-08-28 11:43:25 -0300

    Version bump

--- revelation/__init__.py
+++ revelation/__init__.py
@@ -5,5 +5,5 @@ from .app import PresentationReloader, Revelation
 
 __author__ = "Humberto Rocha"
 __email__ = "humrochagf@gmail.com"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __all__ = ["PresentationReloader", "Revelation"]
